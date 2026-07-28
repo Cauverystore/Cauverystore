b24e7670640cb6d78784db6b2e9c572805f21b49
NoyyalStore <contactvijaykrishna@gmail.com>
2026-07-28 15:10:50 +0530

    fix: re-enable DataSourceAutoConfiguration so Spring Boot creates the datasource bean

--- backend-java/src/main/java/com/cauverystore/CauveryStoreApplication.java
+++ backend-java/src/main/java/com/cauverystore/CauveryStoreApplication.java
@@ -2,11 +2,10 @@ package com.cauverystore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableCaching
 @EnableScheduling
 public class CauveryStoreApplication {
